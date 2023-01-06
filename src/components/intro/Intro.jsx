7d54c033933f82ac574e39594c9a24c01e84f79a
nikhil27apr <71115716+nikhil27apr@users.noreply.github.com>
2023-01-06 15:45:10 +0530

    Update Intro.jsx

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -87,7 +87,7 @@ const Intro = () => {
       </div>
       <div className="i-right">
         <div className="i-bg"></div>
-        <img src={Me} alt="" className="i-img" />
+        <img style={{box-shadow: "10px 10px 5px lightblue"}} src={Me} alt="" className="i-img" />
       </div>
     </div>
   );
